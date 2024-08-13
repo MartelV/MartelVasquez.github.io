a9d7ac519c535b052f15ff86b6d99eb0924f6f23
Martel Vasquez <128270758+MartelV@users.noreply.github.com>
2024-08-13 00:26:38 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,13 +4,31 @@ window.addEventListener('scroll', function() {
     const intro = document.querySelector('.intro');
     const heroHeader = document.querySelector('.hero-header');
 
-    if (scrollY > 0) {
+    if (scrollY > 0 && !intro.classList.contains('expanded')) {
         intro.classList.add('expanded');
 
         setTimeout(function() {
             intro.classList.add('hidden');
-            heroHeader.style.opacity = '1'; // Ensure the header image is visible
+            heroHeader.style.opacity = '1';
+            setTimeout(function() {
+                intro.parentNode.removeChild(intro);
+                window.scrollTo({
+                    top: window.top, // Lock scroll to the hero header
+                    behavior: 'smooth' // Instant scroll without smooth behavior
+                });
+            }, 100); 
         }, 1000); // Delay to allow the expansion to complete before hiding
+    }
+
+    // Remove the intro section from the DOM and lock scroll up after scrolling past the intro
+    if (scrollY >= window.innerHeight) {
+        intro.parentNode.removeChild(intro);
+
+        // Lock scroll up by setting the scroll position to the top of the hero header
+        window.scrollTo({
+            top: window.top,
+            behavior: 'smooth'
+        });
     }
 });
 
